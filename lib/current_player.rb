7447b74d88acc53c24359683e49f81a6d77c623b
Khaled S Momani <khaledmomani4@gmail.com>
2020-11-20 05:11:59 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,19 +1,24 @@
+#Uses conditional, to count number of moves made.
 
+  
 def turn_count(board)
-  turns = 0
-  board.each do |space|
-    if space == "X" || space == "O"
-      turns += 1
-    end
+  turns = 0 
+  board.each do |i|
+  if i < 9
+    turns += 1
   end
-  return turns
 end
 
+return turns
+end
+
+#Determines current player 
+
 def current_player(board)
-  turns = turn_count(board)
-  if turns % 2 == 0
-    return "X"
-  else
-    return "O"
-  end
+  i = turn_count(board)
+if i % 2 == 0
+  puts "X"
+else
+  puts "O"
+end
 end
